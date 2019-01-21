a73d0ec59ccad9d5e1f42786b4b97ad6106a6b1b
Diana <dianadias1999@hotmail.com>
2019-01-21 23:54:05 +0000

    processing a centrar automaticamente

--- JavaScript/sketch.js
+++ JavaScript/sketch.js
@@ -1,6 +1,6 @@
 let altura;
 let largura;
-let marginX,marginY;
+let cv;
 
 let d = [];
 let a = [];
@@ -35,72 +35,73 @@ function preload() {
 }
 
 function setup() {
-    altura = windowHeight - windowWidth*0.07;
+    let processing = select('#processingcanvas');
+    let height1 = processing.style('height');
+    let width1 = processing.style('width');
+    let height2 = float(height1);
+    let width2 = float(width1);
+    altura = height2;
     largura = altura/12*20;
-    marginX = (windowWidth - windowWidth*0.03 - largura)/2;
-    marginY = 0;
-    if(largura >= windowWidth){
-        largura = windowWidth-windowWidth*0.03;
+    if(largura >= width2){
+        largura = width2;
         altura = largura/20*12;
-        marginX = 0;
-        marginY = (windowHeight - windowWidth*0.07 - altura)/2;
     }
-    let cv = createCanvas(largura+marginX, altura+marginY);
+    cv = createCanvas(largura, altura);
     cv.parent("processingcanvas");
-
+    cv.center();
     //inicialização letra d
-    d[0] = new Peca(4, largura/20 + marginX, altura/12*7 + marginY, 1);
-    d[1] = new Peca(2, largura/20 + marginX, altura/12*8 + marginY, 1);
-    d[2] = new Peca(4, largura/20*2 + marginX, altura/12*6 + marginY, 1);
-    d[3] = new Peca(2, largura/20*2 + marginX, altura/12*9 + marginY, 1);
-    d[4] = new Peca(4, largura/20*3 + marginX, altura/12*5 + marginY, 1);
-    d[5] = new Peca(2, largura/20*3 + marginX, altura/12*10 + marginY, 1);
-    d[6] = new Peca(3, largura/20*4 + marginX, altura/12*5 + marginY, 1);
-    d[7] = new Peca(1, largura/20*4 + marginX, altura/12*10 + marginY, 1);
-    d[8] = new Peca(3, largura/20*5 + marginX, altura/12*6 + marginY, 1);
-    d[9] = new Peca(1, largura/20*5 + marginX, altura/12*9 + marginY, 1);
-    d[10] = new Peca(3, largura/20*6 + marginX, altura/12 + marginY, 1);
-    d[11] = new Peca(1, largura/20*6 + marginX, altura/12*2 + marginY, 1);
-    d[12] = new Peca(4, largura/20*5 + marginX, altura/12*3 + marginY, 1);
-    d[13] = new Peca(2, largura/20*5 + marginX, altura/12*4 + marginY, 1);
-    d[14] = new Peca(3, largura/20*6 + marginX, altura/12*5 + marginY, 1);
-    d[15] = new Peca(1, largura/20*6 + marginX, altura/12*6 + marginY, 1);
-    d[16] = new Peca(3, largura/20*6 + marginX, altura/12*7 + marginY, 1);
-    d[17] = new Peca(1, largura/20*6 + marginX, altura/12*8 + marginY, 1);
-    d[18] = new Peca(3, largura/20*6 + marginX, altura/12*9 + marginY, 1);
-    d[19] = new Peca(2, largura/20*6 + marginX, altura/12*10 + marginY, 1);
+    d[0] = new Peca(4, largura/20, altura/12*7, 1);
+    d[1] = new Peca(2, largura/20, altura/12*8, 1);
+    d[2] = new Peca(4, largura/20*2, altura/12*6, 1);
+    d[3] = new Peca(2, largura/20*2, altura/12*9, 1);
+    d[4] = new Peca(4, largura/20*3, altura/12*5, 1);
+    d[5] = new Peca(2, largura/20*3, altura/12*10, 1);
+    d[6] = new Peca(3, largura/20*4, altura/12*5, 1);
+    d[7] = new Peca(1, largura/20*4, altura/12*10, 1);
+    d[8] = new Peca(3, largura/20*5, altura/12*6, 1);
+    d[9] = new Peca(1, largura/20*5, altura/12*9, 1);
+    d[10] = new Peca(3, largura/20*6, altura/12, 1);
+    d[11] = new Peca(1, largura/20*6, altura/12*2, 1);
+    d[12] = new Peca(4, largura/20*5, altura/12*3, 1);
+    d[13] = new Peca(2, largura/20*5, altura/12*4, 1);
+    d[14] = new Peca(3, largura/20*6, altura/12*5, 1);
+    d[15] = new Peca(1, largura/20*6, altura/12*6, 1);
+    d[16] = new Peca(3, largura/20*6, altura/12*7, 1);
+    d[17] = new Peca(1, largura/20*6, altura/12*8, 1);
+    d[18] = new Peca(3, largura/20*6, altura/12*9, 1);
+    d[19] = new Peca(2, largura/20*6, altura/12*10, 1);
 
     //inicialização letra a
-    a[0] = new Peca(4, largura/20*8 + marginX, altura/12*7 + marginY, 2);
-    a[1] = new Peca(2, largura/20*8 + marginX, altura/12*8 + marginY, 2);
-    a[2] = new Peca(4, largura/20*9 + marginX, altura/12*6 + marginY, 2);
-    a[3] = new Peca(2, largura/20*9 + marginX, altura/12*9 + marginY, 2);
-    a[4] = new Peca(4, largura/20*10 + marginX, altura/12*5 + marginY, 2);
-    a[5] = new Peca(2, largura/20*10 + marginX, altura/12*10 + marginY, 2);
-    a[6] = new Peca(3, largura/20*11 + marginX, altura/12*5 + marginY, 2);
-    a[7] = new Peca(1, largura/20*11 + marginX, altura/12*10 + marginY, 2);
-    a[8] = new Peca(3, largura/20*12 + marginX, altura/12*6 + marginY, 2);
-    a[9] = new Peca(1, largura/20*12 + marginX, altura/12*9 + marginY, 2);
-    a[10] = new Peca(1, largura/20*13 + marginX, altura/12*6 + marginY, 2);
-    a[11] = new Peca(3, largura/20*13 + marginX, altura/12*7 + marginY, 2);
-    a[12] = new Peca(1, largura/20*13 + marginX, altura/12*8 + marginY, 2);
-    a[13] = new Peca(3, largura/20*13 + marginX, altura/12*9 + marginY, 2);
-    a[14] = new Peca(2, largura/20*13 + marginX, altura/12*10 + marginY, 2);
-    a[15] = new Peca(1, largura/20*14 + marginX, altura/12*10 + marginY, 2);
+    a[0] = new Peca(4, largura/20*8, altura/12*7, 2);
+    a[1] = new Peca(2, largura/20*8, altura/12*8, 2);
+    a[2] = new Peca(4, largura/20*9, altura/12*6, 2);
+    a[3] = new Peca(2, largura/20*9, altura/12*9, 2);
+    a[4] = new Peca(4, largura/20*10, altura/12*5, 2);
+    a[5] = new Peca(2, largura/20*10, altura/12*10, 2);
+    a[6] = new Peca(3, largura/20*11, altura/12*5, 2);
+    a[7] = new Peca(1, largura/20*11, altura/12*10, 2);
+    a[8] = new Peca(3, largura/20*12, altura/12*6, 2);
+    a[9] = new Peca(1, largura/20*12, altura/12*9, 2);
+    a[10] = new Peca(1, largura/20*13, altura/12*6, 2);
+    a[11] = new Peca(3, largura/20*13, altura/12*7, 2);
+    a[12] = new Peca(1, largura/20*13, altura/12*8, 2);
+    a[13] = new Peca(3, largura/20*13, altura/12*9, 2);
+    a[14] = new Peca(2, largura/20*13, altura/12*10, 2);
+    a[15] = new Peca(1, largura/20*14, altura/12*10, 2);
 
     //inicialização letra t
-    t[0] = new Peca(2, largura/20*15 + marginX, altura/12*5 + marginY, 3);
-    t[1] = new Peca(4, largura/20*16 + marginX, altura/12*4 + marginY, 3);
-    t[2] = new Peca(1, largura/20*16 + marginX, altura/12*5 + marginY, 3);
-    t[3] = new Peca(4, largura/20*16 + marginX, altura/12*7 + marginY, 3);
-    t[4] = new Peca(2, largura/20*16 + marginX, altura/12*8 + marginY, 3);
-    t[5] = new Peca(3, largura/20*17 + marginX, altura/12*2 + marginY, 3);
-    t[6] = new Peca(1, largura/20*17 + marginX, altura/12*3 + marginY, 3);
-    t[7] = new Peca(4, largura/20*17 + marginX, altura/12*5 + marginY, 3);
-    t[8] = new Peca(1, largura/20*17 + marginX, altura/12*6 + marginY, 3);
-    t[9] = new Peca(3, largura/20*17 + marginX, altura/12*9 + marginY, 3);
-    t[10] = new Peca(2, largura/20*17 + marginX, altura/12*10 + marginY, 3);
-    t[11] = new Peca(3, largura/20*18 + marginX, altura/12*5 + marginY, 3);
+    t[0] = new Peca(2, largura/20*15, altura/12*5, 3);
+    t[1] = new Peca(4, largura/20*16, altura/12*4, 3);
+    t[2] = new Peca(1, largura/20*16, altura/12*5, 3);
+    t[3] = new Peca(4, largura/20*16, altura/12*7, 3);
+    t[4] = new Peca(2, largura/20*16, altura/12*8, 3);
+    t[5] = new Peca(3, largura/20*17, altura/12*2, 3);
+    t[6] = new Peca(1, largura/20*17, altura/12*3, 3);
+    t[7] = new Peca(4, largura/20*17, altura/12*5, 3);
+    t[8] = new Peca(1, largura/20*17, altura/12*6, 3);
+    t[9] = new Peca(3, largura/20*17, altura/12*9, 3);
+    t[10] = new Peca(2, largura/20*17, altura/12*10, 3);
+    t[11] = new Peca(3, largura/20*18, altura/12*5, 3);
 }
 
 function draw() {
@@ -131,67 +132,69 @@ function draw() {
 }
 
 function windowResized() {
-    altura = windowHeight - windowWidth*0.07;
+    let processing = select('#processingcanvas');
+    let height1 = processing.style('height');
+    let width1 = processing.style('width');
+    let height2 = float(height1);
+    let width2 = float(width1);
+    altura = height2;
     largura = altura/12*20;
-    marginX = (windowWidth - windowWidth*0.03 - largura)/2;
-    marginY = 0;
-    if(largura >= windowWidth){
-        largura = windowWidth-windowWidth*0.03;
+    if(largura >= width2){
+        largura = width2;
         altura = largura/20*12;
-        marginX = 0;
-        marginY = (windowHeight - windowWidth*0.07 - altura)/2;
     }
-    resizeCanvas(largura+marginX, altura+marginY);
-    d[0].mudarCoord(largura/20 + marginX, altura/12*7 + marginY);
-    d[1].mudarCoord(largura/20 + marginX, altura/12*8 + marginY);
-    d[2].mudarCoord(largura/20*2 + marginX, altura/12*6 + marginY);
-    d[3].mudarCoord(largura/20*2 + marginX, altura/12*9 + marginY);
-    d[4].mudarCoord(largura/20*3 + marginX, altura/12*5 + marginY);
-    d[5].mudarCoord(largura/20*3 + marginX, altura/12*10 + marginY);
-    d[6].mudarCoord(largura/20*4 + marginX, altura/12*5 + marginY);
-    d[7].mudarCoord(largura/20*4 + marginX, altura/12*10 + marginY);
-    d[8].mudarCoord(largura/20*5 + marginX, altura/12*6 + marginY);
-    d[9].mudarCoord(largura/20*5 + marginX, altura/12*9 + marginY);
-    d[10].mudarCoord(largura/20*6 + marginX, altura/12 + marginY);
-    d[11].mudarCoord(largura/20*6 + marginX, altura/12*2 + marginY);
-    d[12].mudarCoord(largura/20*5 + marginX, altura/12*3 + marginY);
-    d[13].mudarCoord(largura/20*5 + marginX, altura/12*4 + marginY);
-    d[14].mudarCoord(largura/20*6 + marginX, altura/12*5 + marginY);
-    d[15].mudarCoord(largura/20*6 + marginX, altura/12*6 + marginY);
-    d[16].mudarCoord(largura/20*6 + marginX, altura/12*7 + marginY);
-    d[17].mudarCoord(largura/20*6 + marginX, altura/12*8 + marginY);
-    d[18].mudarCoord(largura/20*6 + marginX, altura/12*9 + marginY);
-    d[19].mudarCoord(largura/20*6 + marginX, altura/12*10 + marginY);
+    resizeCanvas(largura, altura);
+    cv.center();
+    d[0].mudarCoord(largura/20, altura/12*7);
+    d[1].mudarCoord(largura/20, altura/12*8);
+    d[2].mudarCoord(largura/20*2, altura/12*6);
+    d[3].mudarCoord(largura/20*2, altura/12*9);
+    d[4].mudarCoord(largura/20*3, altura/12*5);
+    d[5].mudarCoord(largura/20*3, altura/12*10);
+    d[6].mudarCoord(largura/20*4, altura/12*5);
+    d[7].mudarCoord(largura/20*4, altura/12*10);
+    d[8].mudarCoord(largura/20*5, altura/12*6);
+    d[9].mudarCoord(largura/20*5, altura/12*9);
+    d[10].mudarCoord(largura/20*6, altura/12);
+    d[11].mudarCoord(largura/20*6, altura/12*2);
+    d[12].mudarCoord(largura/20*5, altura/12*3);
+    d[13].mudarCoord(largura/20*5, altura/12*4);
+    d[14].mudarCoord(largura/20*6, altura/12*5);
+    d[15].mudarCoord(largura/20*6, altura/12*6);
+    d[16].mudarCoord(largura/20*6, altura/12*7);
+    d[17].mudarCoord(largura/20*6, altura/12*8);
+    d[18].mudarCoord(largura/20*6, altura/12*9);
+    d[19].mudarCoord(largura/20*6, altura/12*10);
 
-    a[0].mudarCoord(largura/20*8 + marginX, altura/12*7 + marginY);
-    a[1].mudarCoord(largura/20*8 + marginX, altura/12*8 + marginY);
-    a[2].mudarCoord(largura/20*9 + marginX, altura/12*6 + marginY);
-    a[3].mudarCoord(largura/20*9 + marginX, altura/12*9 + marginY);
-    a[4].mudarCoord(largura/20*10 + marginX, altura/12*5 + marginY);
-    a[5].mudarCoord(largura/20*10 + marginX, altura/12*10 + marginY);
-    a[6].mudarCoord(largura/20*11 + marginX, altura/12*5 + marginY);
-    a[7].mudarCoord(largura/20*11 + marginX, altura/12*10 + marginY);
-    a[8].mudarCoord(largura/20*12 + marginX, altura/12*6 + marginY);
-    a[9].mudarCoord(largura/20*12 + marginX, altura/12*9 + marginY);
-    a[10].mudarCoord(largura/20*13 + marginX, altura/12*6 + marginY);
-    a[11].mudarCoord(largura/20*13 + marginX, altura/12*7 + marginY);
-    a[12].mudarCoord(largura/20*13 + marginX, altura/12*8 + marginY);
-    a[13].mudarCoord(largura/20*13 + marginX, altura/12*9 + marginY);
-    a[14].mudarCoord(largura/20*13 + marginX, altura/12*10 + marginY);
-    a[15].mudarCoord(largura/20*14 + marginX, altura/12*10 + marginY);
+    a[0].mudarCoord(largura/20*8, altura/12*7);
+    a[1].mudarCoord(largura/20*8, altura/12*8);
+    a[2].mudarCoord(largura/20*9, altura/12*6);
+    a[3].mudarCoord(largura/20*9, altura/12*9);
+    a[4].mudarCoord(largura/20*10, altura/12*5);
+    a[5].mudarCoord(largura/20*10, altura/12*10);
+    a[6].mudarCoord(largura/20*11, altura/12*5);
+    a[7].mudarCoord(largura/20*11, altura/12*10);
+    a[8].mudarCoord(largura/20*12, altura/12*6);
+    a[9].mudarCoord(largura/20*12, altura/12*9);
+    a[10].mudarCoord(largura/20*13, altura/12*6);
+    a[11].mudarCoord(largura/20*13, altura/12*7);
+    a[12].mudarCoord(largura/20*13, altura/12*8);
+    a[13].mudarCoord(largura/20*13, altura/12*9);
+    a[14].mudarCoord(largura/20*13, altura/12*10);
+    a[15].mudarCoord(largura/20*14, altura/12*10);
 
-    t[0].mudarCoord(largura/20*15 + marginX, altura/12*5 + marginY);
-    t[1].mudarCoord(largura/20*16 + marginX, altura/12*4 + marginY);
-    t[2].mudarCoord(largura/20*16 + marginX, altura/12*5 + marginY);
-    t[3].mudarCoord(largura/20*16 + marginX, altura/12*7 + marginY);
-    t[4].mudarCoord(largura/20*16 + marginX, altura/12*8 + marginY);
-    t[5].mudarCoord(largura/20*17 + marginX, altura/12*2 + marginY);
-    t[6].mudarCoord(largura/20*17 + marginX, altura/12*3 + marginY);
-    t[7].mudarCoord(largura/20*17 + marginX, altura/12*5 + marginY);
-    t[8].mudarCoord(largura/20*17 + marginX, altura/12*6 + marginY);
-    t[9].mudarCoord(largura/20*17 + marginX, altura/12*9 + marginY);
-    t[10].mudarCoord(largura/20*17 + marginX, altura/12*10 + marginY);
-    t[11].mudarCoord(largura/20*18 + marginX, altura/12*5 + marginY);
+    t[0].mudarCoord(largura/20*15, altura/12*5);
+    t[1].mudarCoord(largura/20*16, altura/12*4);
+    t[2].mudarCoord(largura/20*16, altura/12*5);
+    t[3].mudarCoord(largura/20*16, altura/12*7);
+    t[4].mudarCoord(largura/20*16, altura/12*8);
+    t[5].mudarCoord(largura/20*17, altura/12*2);
+    t[6].mudarCoord(largura/20*17, altura/12*3);
+    t[7].mudarCoord(largura/20*17, altura/12*5);
+    t[8].mudarCoord(largura/20*17, altura/12*6);
+    t[9].mudarCoord(largura/20*17, altura/12*9);
+    t[10].mudarCoord(largura/20*17, altura/12*10);
+    t[11].mudarCoord(largura/20*18, altura/12*5);
 }
 
 
